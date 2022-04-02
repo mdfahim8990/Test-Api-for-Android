431101cb5e046ab4e03e8efa5f9f419d7126bfcd
monjourur <md899042@gmail.com>
2022-04-02 13:10:51 +0600

    ModelClass

--- app/src/main/java/com/example/testapi/ModelClass.java
+++ app/src/main/java/com/example/testapi/ModelClass.java
@@ -9,8 +9,47 @@ public class ModelClass {
     String per_page;
     String total;
     String total_pages;
-
     public ArrayList<data>data;
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public String getPer_page() {
+        return per_page;
+    }
+
+    public void setPer_page(String per_page) {
+        this.per_page = per_page;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(String total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public ArrayList<ModelClass.data> getData() {
+        return data;
+    }
+
+    public void setData(ArrayList<ModelClass.data> data) {
+        this.data = data;
+    }
 
     class data {
 
@@ -20,6 +59,44 @@ public class ModelClass {
         String last_name;
         String avater;
 
+        public String getId() {
+            return id;
+        }
 
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getFirst_name() {
+            return first_name;
+        }
+
+        public void setFirst_name(String first_name) {
+            this.first_name = first_name;
+        }
+
+        public String getLast_name() {
+            return last_name;
+        }
+
+        public void setLast_name(String last_name) {
+            this.last_name = last_name;
+        }
+
+        public String getAvater() {
+            return avater;
+        }
+
+        public void setAvater(String avater) {
+            this.avater = avater;
+        }
     }
 }
