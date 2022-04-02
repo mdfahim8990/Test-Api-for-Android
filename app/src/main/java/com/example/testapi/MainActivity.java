ee277a3cef85351a4a9138547de29f8a7793b6f6
monjourur <md899042@gmail.com>
2022-04-02 17:51:15 +0600

    data get and show in app done

--- app/src/main/java/com/example/testapi/MainActivity.java
+++ app/src/main/java/com/example/testapi/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,8 +16,12 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+
      private  static  String TAG = "MainActivity";
     Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         button = findViewById(R.id.getbutton);
+        textView = findViewById(R.id.TextView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,19 +45,37 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(Call<ModelClass> call, Response<ModelClass> response) {
                         Log.e(TAG, "Response Code :"+response.code());
                        /* ArrayList<ModelClass.data>data = response.body().getData;*/
+
+                        if (!response.isSuccessful()){
+                            textView.setText(""+response.code());
+                            return;
+                        }
+
                         ArrayList<ModelClass.data> data = response.body().getData();
 
                         for (ModelClass.data data1 : data){
-                            Log.e(TAG, "Response Code :"+data1.getFirst_name());
+                            //Log.e(TAG, "Response Name :"+data1.getFirst_name());
+                            String content="";
+                            content += " " + data1.getFirst_name() + "\n";
+                            content += " " + data1.getLast_name() + "\n";
+                            content += " " + data1.getEmail() + "\n\n";
+                            textView.append(content);
+
+
                         }
+
+
                     }
 
                     @Override
                     public void onFailure(Call<ModelClass> call, Throwable t) {
 
+                        textView.setText(t.getMessage());
+
                     }
                 });
             }
         });
+
     }
 }
